8eedb3dffc98effb27599df06f839d093389ed21
shirleyahn <sahn@ucdavis.edu>
2024-05-21 19:47:36 -0700

    added binding site files

---
+++ ecoli/oxygen_channel/pdf.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pylab as pylab
+from matplotlib.ticker import AutoMinorLocator
+params = {'font.family': 'sans-serif',
+          'font.sans-serif': 'Helvetica',
+         'legend.fontsize': '36',
+         'figure.figsize': (24,12),
+         'axes.labelsize': '36',
+         'axes.titlesize': '36',
+         'xtick.labelsize': '36',
+         'ytick.labelsize': '36'}
+pylab.rcParams.update(params)
+
+dist = np.loadtxt('POVME_run/example_volumes.tabbed.txt')
+dist = dist[:, 1]
+
+fig = plt.figure(figsize=(24,12))
+ax = fig.add_subplot(111)
+numBins = 50
+print(np.max(dist), np.min(dist), np.mean(dist))
+n, bins, patches = plt.hist(dist, numBins, label='E. coli oxygen channel', color='dodgerblue', alpha=0.8, rwidth=0.9, density=False, range=(0.0, 2600.0))
+print(n, bins, patches)
+ax.set_xlabel('Volume ($\AA^3$)',labelpad=20)
+ax.set_ylabel('# of Structures',labelpad=20)
+plt.legend(loc=0)
+fig.savefig('Figure.png', bbox_inches='tight')
+
